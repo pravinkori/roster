a3d03492e7ae3bb57dd631b176602d613ac37ffe
Pravin Kori <pravinkori@protonmail.ch>
2025-05-19 19:08:35 +0530

    feat: Add file creation functionality

---
+++ src/file.c
@@ -0,0 +1,24 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "common.h"
+#include "file.h"
+
+int create_db_file(char *filename) {
+    // check if file already exist
+    int fd = open(filename, O_RDONLY);
+    if (fd != -1) {
+        close(fd);
+        printf("File already exists\n");
+        return STATUS_ERROR;
+    }
+    // if specified file doesn't exist, create the file
+    fd = open(filename, O_RDWR | O_CREAT, 0644);
+    if (fd == -1) {
+        perror("open");
+        return STATUS_ERROR;
+    }
+
+    return fd;
+}
