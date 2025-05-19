bd59cb6103995325a605abcb426a335b1511de2c
Pravin Kori <pravinkori@protonmail.ch>
2025-05-19 18:13:06 +0530

    feat: Scaffolding main.c with -f and -n flags

---
+++ src/main.c
@@ -0,0 +1,47 @@
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "common.h"
+#include "file.h"
+#include "parse.h"
+
+void print_usage(char *argv[]) {
+    printf("Usage: %s -n -f <database file>\n", argv[0]);
+    printf("\t -n: create new database file\n");
+    printf("\t -f: (required) path to database file\n");
+    return;
+}
+
+int main(int argc, char *argv[]) {
+    char *filepath = NULL;
+    bool newfile = false;
+    int c; // contain the current case that we are on
+
+    while ((c = getopt(argc, argv, "nf:")) != -1) {
+        switch (c) {
+        case 'n':
+            newfile = true;
+            break;
+        case 'f':
+            filepath = optarg;
+            break;
+        case '?':
+            printf("Unknown option -%c\n", c);
+            break;
+        default:
+            return -1;
+        }
+    }
+
+    if (filepath == NULL) {
+        printf("Filepath is a required argument\n");
+        print_usage(argv);
+        return 0;
+    }
+
+    printf("Newfile: %d\n", newfile);
+    printf("File path: %s\n", filepath);
+
+    return 0;
+}
